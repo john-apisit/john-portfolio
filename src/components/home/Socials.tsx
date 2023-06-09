d7d2a8f2c5f0c0fa6ac6e52e6913d751dabcd065
Apisit Sianglert <john.apisits@gmail.com>
2023-06-09 14:23:34 +0700

    Remove Facebook

--- src/components/home/Socials.tsx
+++ src/components/home/Socials.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 import {
   FaLinkedin,
   FaFacebookSquare,
   FaRegFileAlt,
-  FaGithub
-} from 'react-icons/fa'
+  FaGithub,
+} from "react-icons/fa";
 
 const Socials = ({ className }: { className?: string }) => {
   return (
-    <div
-      className={`${className} gap-4 justify-center text-gray-300 z-50`}
-    >
+    <div className={`${className} gap-4 justify-center text-gray-300 z-50`}>
       <a
         target="_blank"
         href="https://www.linkedin.com/in/apisit-sianglert"
@@ -18,13 +16,13 @@ const Socials = ({ className }: { className?: string }) => {
       >
         <FaLinkedin className="text-primary cursor-pointer" size={28} />
       </a>
-      <a
+      {/* <a
         target="_blank"
         href="https://www.facebook.com/pha.john.14"
         className="hover:animate-pulse hover:scale-110"
       >
         <FaFacebookSquare className="text-primary cursor-pointer" size={28} />
-      </a>
+      </a> */}
       <a
         target="_blank"
         href="https://github.com/johns-portfolio"
@@ -33,7 +31,7 @@ const Socials = ({ className }: { className?: string }) => {
         <FaGithub className="text-primary cursor-pointer" size={28} />
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
